ad4b305b1783f5b1898bb03707562c8e0d9da734
getPoland <36799117+getPoland@users.noreply.github.com>
2018-08-15 13:10:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
-if(isset($_POST['args'])){
-$string = $_POST['args'];
-
+  if(isset($_POST['args'])){
+    $string = $_POST['args'];
 ?>
-<div class="container">
-  <form method="POST" src="index.php">
-  <input type="text" placeholder="Search..." name="args" required>
-    <input type="submit" hidden>
-    </form>
-  <?php
-  echo 'Search query =' . $string;
-}
-?>
-  <div class="search"></div>
-</div>
+<DOCTYPE HTML>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+  </head>
+  <body>
+    <div class="container">
+      <form method="POST" src="index.php">
+        <input type="text" placeholder="Search..." name="args" required>
+        <input type="submit" hidden>
+      </form>
+    <?php
+      echo 'Search query =' . $string;
+    }
+    ?>
+    <div class="search"></div>
+    </div>
+  </body>
+</html
